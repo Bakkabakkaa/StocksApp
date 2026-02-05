095ad24b6c04171365c226a2fd14736f813d9d74
Art'm <kasko.artem45@gmail.com>
2026-02-06 02:59:06 +0300

    Add Validate method to DateAndTimeOfOrder Validation

--- ServiceContracts/DTO/SellOrderRequest.cs
+++ ServiceContracts/DTO/SellOrderRequest.cs
@@ -52,4 +52,22 @@ public class SellOrderRequest
             DateAndTimeOfOrder = this.DateAndTimeOfOrder
         };
     }
+
+    /// <summary>
+    /// Model class-level validation using IValidatableObject
+    /// </summary>
+    /// <param name="validationContext">ValidationContext to validate</param>
+    /// <returns>Returns validation errors as ValidationResult</returns>
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        List<ValidationResult> results = new List<ValidationResult>();
+        
+        // Date of order should be less than 01.01.2000
+        if (DateAndTimeOfOrder < Convert.ToDateTime("2000-01-01"))
+        {
+            results.Add(new ValidationResult("Date of the order should not be older than Jan 01, 2000."));
+        }
+
+        return results;
+    }
 }
