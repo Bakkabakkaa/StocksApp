8260bb9d2ba5c03a3bd02dae9d56c0ae3eb3de4f
Art'm <kasko.artem45@gmail.com>
2026-02-07 01:46:11 +0300

    Create model validate ValidationHelpers.cs

---
+++ Services/Helpers/ValidationHelpers.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Services.Helpers;
+
+public class ValidationHelpers
+{
+    /// <summary>
+    /// Persons model validationContext and throws ArgumentException in case
+    /// of any validation errors
+    /// </summary>
+    /// <param name="obj"></param>
+    /// <exception cref="ArgumentException">When one or more validation errors found</exception>
+
+    internal static void ModelValidation(object obj)
+    {
+        // Model validation
+        ValidationContext validationContext = new ValidationContext(obj);
+        List<ValidationResult> validationResults = new List<ValidationResult>();
+        
+        // Validate the model object and get errors
+        bool isValid = Validator.TryValidateObject(obj, validationContext, validationResults);
+
+        if (!isValid)
+        {
+            throw new ArgumentException(validationResults.FirstOrDefault()?.ErrorMessage);
+        }
+    }
+}
