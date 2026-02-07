f7f660fb5f0680b6819ef97446a43e6b294d35ce
Art'm <kasko.artem45@gmail.com>
2026-02-07 16:30:39 +0300

    Configure stock services and MVC middleware

--- StockMarketSolution/Program.cs
+++ StockMarketSolution/Program.cs
@@ -1,6 +1,19 @@
+using ServiceContracts;
+using Services;
+using StockMarketSolution;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.Configure<TradingOptions>(builder.Configuration.GetSection("TradingOptions"));
+builder.Services.AddSingleton<IStockService, StockService>();
+builder.Services.AddSingleton<IFinnhubService, FinnhubService>();
+builder.Services.AddHttpClient();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
+app.MapControllers();
+app.UseRouting();
 
 app.Run();
