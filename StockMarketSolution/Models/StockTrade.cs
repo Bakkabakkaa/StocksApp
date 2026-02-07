c7a7fc308c7fbf6ff7e50c43f6c4947d89dd1637
Art'm <kasko.artem45@gmail.com>
2026-02-07 16:02:02 +0300

    Create StockTrade.cs

---
+++ StockMarketSolution/Models/StockTrade.cs
@@ -0,0 +1,14 @@
+namespace StockMarketSolution.Models;
+
+/// <summary>
+/// Represents the model class to supply trade details
+/// (stock id, stock name, price and quantity etc.)
+/// to the Trade/Index view
+/// </summary>
+public class StockTrade
+{
+    public string StockSymbol { get; set; }
+    public string StockName { get; set; }
+    public double Price { get; set; }
+    public uint Quantity { get; set; }
+}
