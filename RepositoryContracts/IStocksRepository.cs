63246e16e32d03b77367ef5bfacc42decdfbe371
Art'm <kasko.artem45@gmail.com>
2026-02-19 00:02:18 +0300

    Add basic methods to the IStocksRepository

--- RepositoryContracts/IStocksRepository.cs
+++ RepositoryContracts/IStocksRepository.cs
@@ -1,6 +1,37 @@
+using Entities;
+
 namespace RepositoryContracts;
 
+/// <summary>
+/// Represents Stocks service that includes operation like buy order, sell order
+/// </summary>
 public interface IStocksRepository
 {
-    
+    /// <summary>
+    /// Creates a buy order
+    /// </summary>
+    /// <param name="buyOrder">Buy order object</param>
+    /// <returns></returns>
+    Task<BuyOrder> CreateBuyOrder(BuyOrder buyOrder);
+
+    /// <summary>
+    /// Creates a sell order
+    /// </summary>
+    /// <param name="sellOrder"></param>
+    /// <returns></returns>
+    Task<SellOrder> CreateSellOrder(SellOrder sellOrder);
+
+    /// <summary>
+    /// Returns all existing buy orders
+    /// </summary>
+    /// <returns>Returns a list of objects of BuyOrder typ</returns>
+    Task<List<BuyOrder>> GetBuyOrders();
+
+    /// <summary>
+    /// Returns all existing sell orders
+    /// </summary>
+    /// <returns>Returns a list of objects of SellOrder type</returns>
+    Task<List<SellOrder>> GetSellOrders();
+
+
 }
